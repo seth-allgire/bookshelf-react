f68eb086691267c47da8cf9f5e0a22556d60d8ae
Seth Allgire <seth.allgire@gmail.com>
2021-11-02 14:08:46 -0500

    adds useAxios  and title search

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import useAxios from "../hooks/useAxios";
+import { BookContext } from "../shared/BookContext";
 
-function SearchPage() {
-  return <div>Search Page</div>;
+const bookURL = `http://openlibrary.org/search.json?title=
+`;
+
+export default function SearchPage() {
+  const { myBooks, addMyBook, deleteMyBook, search, setSearch } =
+    useContext(BookContext);
+  const [queryInput, setQueryInput] = useState("");
+  const [query, setQuery] = useState("");
+  // const [formError, setFormError] = useState(false);
+  const { json, error, loading } = useAxios(query, "get");
+
+  useEffect(() => {
+    if (json) {
+      setSearch(() =>
+        json.docs.map((book) => ({
+          key: book.key,
+          title: book.title,
+          author: book.author_name,
+          isbn: book.isbn,
+        }))
+      );
+    } else if (json === error) return;
+
+    console.log(json);
+  }, [json, setSearch, error]);
+
+  return (
+    <div>
+      <input
+        className="form-input"
+        value={queryInput}
+        onChange={(e) => setQueryInput(e.target.value)}
+        id="search"
+        name="search"
+        placeholder="enter title here"
+      ></input>
+      <button
+        // variant="contained"
+        // color="primary"
+        onClick={() => {
+          // if (queryInput.length < 3) {
+          //   setFormError(true);
+          //   return;
+          // }
+          setQuery(bookURL + queryInput);
+        }}
+      >
+        Search
+      </button>
+    </div>
+  );
 }
-
-export default SearchPage;
--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,8 +1,34 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
-function useAxios() {
-  return <div></div>;
-}
+export default function useAxios(url, method, body = null) {
+  const [json, setJson] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-export default useAxios;
+  useEffect(() => {
+    async function init() {
+      if (!url || (method === "post" && !body)) {
+        return;
+      }
+      setLoading(true);
+      setJson(null);
+      setError(null);
+      try {
+        const response = await axios[method](url, body);
+        if (response.data.error) {
+          setError(response.data.error);
+          return;
+        }
+        setJson(response.data);
+      } catch (e) {
+        setError(e);
+      } finally {
+        setLoading(false);
+      }
+    }
+    init();
+  }, [url, body, method]);
+
+  return { json, error, loading };
+}
