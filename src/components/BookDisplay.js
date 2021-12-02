90a6a89acfee30b5c2ad363bc313e4b2073047d1
Seth Allgire <seth.allgire@gmail.com>
2021-12-02 09:17:47 -0600

    books display in rows on search

--- src/components/BookDisplay.js
+++ src/components/BookDisplay.js
@@ -11,6 +11,8 @@ import {
   Tooltip,
   Popover,
   TextField,
+  Grid,
+  Box,
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
@@ -27,6 +29,7 @@ function BookDisplay({
   isMyBook,
   addMyBook,
   deleteMyBook,
+  addBookNote,
 }) {
   const coverURL = "https://covers.openlibrary.org/b/id/" + cover_id + "-M.jpg";
   const infoURL = "https://openlibrary.org" + book_id;
@@ -49,125 +52,150 @@ function BookDisplay({
   const id = open ? "simple-popover" : undefined;
 
   return (
-    <div className="book-container">
-      <Card sx={{ width: "300px", position: "relative" }}>
-        <CardContent>
-          <Typography fontWeight="600">{title}</Typography>
-          <Accordion disableGutters>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-            >
-              <Typography variant="subtitle2">
-                {cover_id && (
-                  <img
-                    className="cover-image"
-                    src={coverURL}
-                    alt="Book Cover"
-                    title="Book Cover"
-                  ></img>
-                )}
-                {!cover_id && "Cover Image Not Available"}
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails sx={{ paddingTop: "0px", paddingBottom: "0px" }}>
-              <Typography variant="body2">By: {author}</Typography>
-            </AccordionDetails>
-            <AccordionDetails>
-              <Typography variant="body2">
-                First Published: {published}
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-        </CardContent>
-        <CardActions>
-          <Tooltip title="summary" placement="top">
-            <IconButton
-              sx={{ position: "absolute", right: "70px", marginBottom: "15px" }}
-              variant="contained"
-              size="large"
-              target="_blank"
-              href={infoURL}
-            >
-              <InfoIcon color="info" />
-            </IconButton>
-          </Tooltip>
+    // <div className="book-container">
+    // <Grid container spacing={4}>
+    // <Grid xs={3}>
+    // <Box sx={{ display: "flex", flexWrap: "wrap", width: "100vw" }}>
+    <>
+      {cover_id && title.length < 34 && (
+        <Card
+          sx={{
+            width: "300px",
+            position: "relative",
+            bgcolor: "#b8e3f3b4",
+            marginLeft: "5px",
+            marginBottom: "5px",
+          }}
+        >
+          <CardContent>
+            <Typography fontWeight="600">{title}</Typography>
+            <Accordion disableGutters>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography variant="subtitle2">
+                  {cover_id && (
+                    <img
+                      className="cover-image"
+                      src={coverURL}
+                      alt="Book Cover"
+                      title="Book Cover"
+                    ></img>
+                  )}
+                  {!cover_id && "Cover Image Not Available"}
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails
+                sx={{ paddingTop: "0px", paddingBottom: "0px" }}
+              >
+                <Typography variant="body2">By: {author}</Typography>
+              </AccordionDetails>
+              <AccordionDetails>
+                <Typography variant="body2">
+                  First Published: {published}
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          </CardContent>
+          <CardActions>
+            <Tooltip title="summary" placement="top">
+              <IconButton
+                sx={{
+                  position: "absolute",
+                  right: "70px",
+                  marginBottom: "15px",
+                }}
+                variant="contained"
+                size="large"
+                target="_blank"
+                href={infoURL}
+              >
+                <InfoIcon color="info" />
+              </IconButton>
+            </Tooltip>
 
-          {!isMyBook && (
-            <Tooltip title="add to shelf" placement="top">
+            {!isMyBook && (
+              <Tooltip title="add to shelf" placement="top">
+                <IconButton
+                  sx={{
+                    position: "absolute",
+                    right: "40px",
+                    marginBottom: "15px",
+                  }}
+                  size="large"
+                  onClick={() =>
+                    addMyBook({
+                      title,
+                      author,
+                      published,
+                      cover_id,
+                      book_id,
+                    })
+                  }
+                >
+                  <AddCircleIcon color="success" />
+                </IconButton>
+              </Tooltip>
+            )}
+            {isMyBook && (
+              <Tooltip title="Remove from shelf" placement="top">
+                <IconButton
+                  sx={{
+                    position: "absolute",
+                    right: "40px",
+                    marginBottom: "15px",
+                  }}
+                  size="large"
+                  onClick={() => deleteMyBook(cover_id)}
+                >
+                  <RemoveCircleIcon color="error" />
+                </IconButton>
+              </Tooltip>
+            )}
+            {isMyBook && (
               <IconButton
-                sx={{
-                  position: "absolute",
-                  right: "40px",
-                  marginBottom: "15px",
-                }}
                 size="large"
-                onClick={() =>
-                  addMyBook({
-                    title,
-                    author,
-                    published,
-                    cover_id,
-                    book_id,
-                  })
-                }
+                aria-describedby={id}
+                onClick={handleClick}
               >
-                <AddCircleIcon color="success" />
+                <CreateIcon sx={{ color: "#FFA500" }} />
               </IconButton>
-            </Tooltip>
-          )}
-          {isMyBook && (
-            <Tooltip title="Remove from shelf" placement="top">
-              <IconButton
-                sx={{
-                  position: "absolute",
-                  right: "40px",
-                  marginBottom: "15px",
-                }}
-                size="large"
-                onClick={() => deleteMyBook(cover_id)}
-              >
-                <RemoveCircleIcon color="error" />
-              </IconButton>
-            </Tooltip>
-          )}
-          {isMyBook && (
-            <IconButton
-              size="large"
-              aria-describedby={id}
-              onClick={handleClick}
+            )}
+            <Popover
+              id={id}
+              open={open}
+              anchorEl={anchorEl}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "center",
+              }}
+              transformOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
             >
-              <CreateIcon sx={{ color: "#FFA500" }} />
-            </IconButton>
-          )}
-          <Popover
-            id={id}
-            open={open}
-            anchorEl={anchorEl}
-            onClose={handleClose}
-            anchorOrigin={{
-              vertical: "top",
-              horizontal: "center",
-            }}
-            transformOrigin={{
-              vertical: "bottom",
-              horizontal: "left",
-            }}
-          >
-            <TextField
-              id="standard-helperText"
-              label="Book Notes"
-              multiline
-              maxRows={6}
-              value={bookNote}
-              onChange={handleChange}
-              sx={{ marginTop: "7px", outline: "none" }}
-            />
-          </Popover>
-        </CardActions>
-      </Card>
-    </div>
+              <TextField
+                id="standard-helperText"
+                label="Book Notes"
+                multiline
+                maxRows={6}
+                value={bookNote}
+                onChange={handleChange}
+                sx={{ marginTop: "7px", outline: "none" }}
+              />
+              <button onClick={() => addBookNote({ bookNote })}>Save</button>
+            </Popover>
+          </CardActions>
+        </Card>
+      )}
+    </>
+
+    // </Box>
+    // </Grid>
+    // </Grid>
   );
 }
 
--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -2,14 +2,24 @@ import React, { useContext, useEffect, useState } from "react";
 import useAxios from "../hooks/useAxios";
 import { BookContext } from "../shared/BookContext";
 import BookDisplay from "./BookDisplay";
-import { CircularProgress, Button, Pagination } from "@mui/material";
+import {
+  CircularProgress,
+  Button,
+  Box,
+  Grid,
+  Avatar,
+  TextField,
+  Typography,
+  Container,
+} from "@mui/material";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 
 const bookURL = `http://openlibrary.org/search.json?title=
 `;
 // const infoURL = "https://openlibrary.org" + bookId + ".json";
 
 export default function SearchPage() {
-  const { myBooks, addMyBook, deleteMyBook, search, setSearch } =
+  const { myBooks, addMyBook, deleteMyBook, addBookNote, search, setSearch } =
     useContext(BookContext);
   const [queryInput, setQueryInput] = useState("");
   const [query, setQuery] = useState("");
@@ -35,64 +45,172 @@ export default function SearchPage() {
     console.log(json);
   }, [json, setSearch, error]);
 
-  return (
-    <div>
-      <input
-        className="form-input"
-        value={queryInput}
-        onChange={(e) => setQueryInput(e.target.value)}
-        id="search"
-        name="search"
-        placeholder="enter title here"
-      ></input>
-      <Button
-        onClick={() => {
-          setQuery(bookURL + queryInput);
-          setAuthorQuery("");
-        }}
-      >
-        Search
-      </Button>
-      {loading && (
-        <>
-          <CircularProgress sx={{}} />
-          <div>Loading</div>
-        </>
-      )}
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
 
-      {search.length > 0 && !loading && (
-        <>
-          <label htmlFor="author">Filter by Author:</label>
-          <input
-            className="form-input"
-            value={authorQuery}
-            onChange={(e) => setAuthorQuery(e.target.value)}
-            id="author"
-            name="author"
-            placeholder="author name"
-          ></input>
-        </>
-      )}
-      {search &&
-        !loading &&
-        search
-          .filter((val) =>
-            val.author.toLowerCase().includes(authorQuery.toLowerCase())
-          )
-          .map((val) => (
-            <BookDisplay
-              isMyBook={myBooks.some((book) => book.cover_id === val.cover_id)}
-              key={val.cover_id}
-              book_id={val.book_id}
-              title={val.title}
-              author={val.author}
-              cover_id={val.cover_id}
-              published={val.published}
-              addMyBook={addMyBook}
-              deleteMyBook={deleteMyBook}
-            ></BookDisplay>
-          ))}
-      {search.length > 0 && <Pagination count={10} />}
-    </div>
+  return (
+    <>
+      <Container component="main">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "#1A6A86" }}>
+            <SearchOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Search
+          </Typography>
+        </Box>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                sx={{ mb: "3px" }}
+                required
+                // fullWidth
+                id="search"
+                label="Book Title/Keyword"
+                name="search"
+                autoComplete="search"
+                value={queryInput}
+                onChange={(e) => setQueryInput(e.target.value)}
+                helperText="Enter book title or keyword"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            // fullWidth
+            variant="contained"
+            sx={{
+              mt: "15px",
+              mb: 2,
+              bgcolor: "#1A6A86",
+              "&:hover": {
+                background: "#1a6986bb",
+              },
+            }}
+            onClick={() => {
+              setQuery(bookURL + queryInput);
+              setAuthorQuery("");
+            }}
+          >
+            Search
+          </Button>
+          {loading && (
+            <>
+              <CircularProgress sx={{}} />
+              <div>Loading</div>
+            </>
+          )}
+          {search.length > 0 && !loading && (
+            <TextField
+              // fullWidth
+              id="author"
+              label="Author"
+              name="author"
+              value={authorQuery}
+              onChange={(e) => setAuthorQuery(e.target.value)}
+              helperText="Filter by author"
+            />
+          )}
+        </Box>
+      </Container>
+      <Box sx={{ display: "flex", flexWrap: "wrap", width: "100vw" }}>
+        {search &&
+          !loading &&
+          search
+            .filter((val) =>
+              val.author.toLowerCase().includes(authorQuery.toLowerCase())
+            )
+            .map((val) => (
+              // <Box sx={{ display: "flex", flexWrap: "wrap", width: "100vw" }}>
+              //   <Grid container spacing={2}>
+              //     <Grid item xs={4}>
+              <BookDisplay
+                isMyBook={myBooks.some(
+                  (book) => book.cover_id === val.cover_id
+                )}
+                key={val.cover_id}
+                book_id={val.book_id}
+                title={val.title}
+                author={val.author}
+                cover_id={val.cover_id}
+                published={val.published}
+                addMyBook={addMyBook}
+                deleteMyBook={deleteMyBook}
+                addBookNote={addBookNote}
+              ></BookDisplay>
+              //     </Grid>
+              //   </Grid>
+              // </Box>
+            ))}
+      </Box>
+    </>
+    // <div>
+    //   <input
+    //     className="form-input"
+    //     value={queryInput}
+    //     onChange={(e) => setQueryInput(e.target.value)}
+    //     id="search"
+    //     name="search"
+    //     placeholder="enter title here"
+    //   ></input>
+    //   <Button
+    //     onClick={() => {
+    //       setQuery(bookURL + queryInput);
+    //       setAuthorQuery("");
+    //     }}
+    //   >
+    //     Search
+    //   </Button>
+    //   {loading && (
+    //     <>
+    //       <CircularProgress sx={{}} />
+    //       <div>Loading</div>
+    //     </>
+    //   )}
+
+    //   {search.length > 0 && !loading && (
+    //     <>
+    //       <label htmlFor="author">Filter by Author:</label>
+    //       <input
+    //         className="form-input"
+    //         value={authorQuery}
+    //         onChange={(e) => setAuthorQuery(e.target.value)}
+    //         id="author"
+    //         name="author"
+    //         placeholder="author name"
+    //       ></input>
+    //     </>
+    //   )}
+    //   {search &&
+    //     !loading &&
+    //     search
+    //       .filter((val) =>
+    //         val.author.toLowerCase().includes(authorQuery.toLowerCase())
+    //       )
+    //       .map((val) => (
+    //         <BookDisplay
+    //           isMyBook={myBooks.some((book) => book.cover_id === val.cover_id)}
+    //           key={val.cover_id}
+    //           book_id={val.book_id}
+    //           title={val.title}
+    //           author={val.author}
+    //           cover_id={val.cover_id}
+    //           published={val.published}
+    //           addMyBook={addMyBook}
+    //           deleteMyBook={deleteMyBook}
+    //           addBookNote={addBookNote}
+    //         ></BookDisplay>
+    //       ))}
+    //   {search.length > 0 && <Pagination count={10} />}
+    // </div>
   );
 }
